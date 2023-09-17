7d4d76c7ff825e56df1961b34cf4d678dccfef8c
hayato94087 <hayato94087@gmail.com>
2023-09-17 21:51:24 +0900

    パッケージを更新し利用

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
-import { add } from "math-helpers-arithmetics";
+import { add, multiply } from "math-helpers-arithmetics";
 
 export default function Page(): JSX.Element {
   return (
     <>
       <h1 className="text-xl">Web</h1>
       <p>1 + 2 = {add(1, 2)}</p>
+      <p>2 * 3 = {multiply(2, 3)}</p>
     </>
   );
 }
